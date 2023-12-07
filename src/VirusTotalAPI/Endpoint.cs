23845a84326fbaf79f0e84a135e7dd7f4c5472d4
hunterlan <konstantin.sharykin@outlook.com>
2023-12-07 17:40:49 +0100

    First steps

---
+++ src/VirusTotalAPI/Endpoint.cs
@@ -0,0 +1,22 @@
+﻿using RestSharp;
+
+namespace VirusTotalAPI;
+
+public abstract class Endpoint
+{
+    protected RestClient _client;
+    protected string _url = "https://www.virustotal.com/api/v3";
+    protected string ApiKey
+    {
+        get => ApiKey;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Api key shouldn't be empty.");
+            }
+
+            ApiKey = value;
+        }
+    }
+}
