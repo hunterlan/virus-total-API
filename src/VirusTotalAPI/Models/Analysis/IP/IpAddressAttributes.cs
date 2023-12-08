c8656096d5e021edad246d7f8fe00efa2b8a94d8
hunterlan <konstantin.sharykin@outlook.com>
2023-12-08 19:48:05 +0100

    Fix naming

--- src/VirusTotalAPI/Models/Analysis/IP/IpAddressAttributes.cs
+++ src/VirusTotalAPI/Models/Analysis/IP/IpAddressAttributes.cs
@@ -46,7 +46,7 @@ public class IpAddressAttributes
     public int? LastHttpsCertificateDate { get; set; }
     
     //TODO: Here they are played themselves, because keys starts from Uppercase, what is problem for me
-    public Dictionary<string, EngineAnalysisResult> LastAnalysisResult { get; set; }
+    public Dictionary<string, EngineAnalysisResult> LastAnalysisResults { get; set; }
     
     public required string RegionalInternetRegistry { get; set; }
     
