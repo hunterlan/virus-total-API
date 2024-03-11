f75069e26f86b226b6846d67b495b137631fd8e2
hunterlan <konstantin.sharykin@outlook.com>
2024-03-11 21:29:02 +0100

    Update file test folder name

--- tests/VirusTotalCore.Tests/FilesTest.cs
+++ tests/VirusTotalCore.Tests/FilesTest.cs
@@ -1,15 +1,19 @@
 ﻿using Microsoft.Extensions.Configuration;
+using RestSharp;
 using VirusTotalCore.Endpoints;
+using Xunit.Abstractions;
 
 namespace VirusTotalCore.Tests;
 
 public class FilesTest
 {
+    private readonly ITestOutputHelper _testOutputHelper;
     private string ApiKey { get; }
     private FilesEndpoint Endpoint { get; }
 
-    public FilesTest()
+    public FilesTest(ITestOutputHelper testOutputHelper)
     {
+        _testOutputHelper = testOutputHelper;
         var settings = new ConfigurationBuilder()
             .AddEnvironmentVariables()
             .AddJsonFile("appsettings.json", optional: true)
@@ -21,7 +25,8 @@ public class FilesTest
     [Fact]
     public async Task PostSmallFile()
     {
-        var analysisResult = await Endpoint.PostFile($"TestFiles{Path.DirectorySeparatorChar}123.txt", null, new CancellationToken());
+        _testOutputHelper.WriteLine(Directory.GetCurrentDirectory());
+        var analysisResult = await Endpoint.PostFile($"test_files{Path.DirectorySeparatorChar}123.txt", null, new CancellationToken());
         Assert.True(analysisResult is not null);
     }
 
